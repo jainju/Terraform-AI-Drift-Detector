5c50237c6c8fc91578da22508715b0bafc4000de
juhi <jjain4@pmintl.net>
2026-06-19 22:52:07 +0530

    adding updates

--- src/drift_detector/providers/aws_provider.py
+++ src/drift_detector/providers/aws_provider.py
@@ -815,12 +815,70 @@ class AWSProvider:
         except Exception:
             pass
 
+        # Get key schema
+        key_schema = table.get("KeySchema", [])
+        hash_key = ""
+        range_key = ""
+        for key in key_schema:
+            if key.get("KeyType") == "HASH":
+                hash_key = key.get("AttributeName", "")
+            elif key.get("KeyType") == "RANGE":
+                range_key = key.get("AttributeName", "")
+
+        # Get GSI count (detect manually added indexes)
+        gsi_count = len(table.get("GlobalSecondaryIndexes", []))
+        lsi_count = len(table.get("LocalSecondaryIndexes", []))
+
+        # Get stream specification
+        stream_spec = table.get("StreamSpecification", {})
+        stream_enabled = stream_spec.get("StreamEnabled", False)
+        stream_view_type = stream_spec.get("StreamViewType", "")
+
+        # Get TTL
+        ttl_enabled = False
+        ttl_attribute = ""
+        try:
+            ttl_resp = dynamodb.describe_time_to_live(TableName=table_name)
+            ttl_desc = ttl_resp.get("TimeToLiveDescription", {})
+            ttl_enabled = ttl_desc.get("TimeToLiveStatus") == "ENABLED"
+            ttl_attribute = ttl_desc.get("AttributeName", "")
+        except Exception:
+            pass
+
+        # Get point-in-time recovery
+        pitr_enabled = False
+        try:
+            pitr_resp = dynamodb.describe_continuous_backups(TableName=table_name)
+            pitr_desc = pitr_resp.get("ContinuousBackupsDescription", {})
+            pitr_status = pitr_desc.get("PointInTimeRecoveryDescription", {}).get(
+                "PointInTimeRecoveryStatus", ""
+            )
+            pitr_enabled = pitr_status == "ENABLED"
+        except Exception:
+            pass
+
+        # Get encryption
+        sse_description = table.get("SSEDescription", {})
+        sse_enabled = sse_description.get("Status") == "ENABLED"
+        sse_type = sse_description.get("SSEType", "")
+
         attributes = {
             "name": table.get("TableName"),
             "billing_mode": table.get("BillingModeSummary", {}).get("BillingMode", "PROVISIONED"),
             "table_status": table.get("TableStatus"),
             "item_count": table.get("ItemCount"),
             "table_size_bytes": table.get("TableSizeBytes"),
+            "hash_key": hash_key,
+            "range_key": range_key,
+            "gsi_count": gsi_count,
+            "lsi_count": lsi_count,
+            "stream_enabled": stream_enabled,
+            "stream_view_type": stream_view_type,
+            "ttl_enabled": ttl_enabled,
+            "ttl_attribute_name": ttl_attribute,
+            "point_in_time_recovery_enabled": pitr_enabled,
+            "server_side_encryption_enabled": sse_enabled,
+            "server_side_encryption_type": sse_type,
         }
 
         return ResourceMetadata(
@@ -849,12 +907,55 @@ class AWSProvider:
         cluster = response.get("cluster", {})
         tags = cluster.get("tags", {})
 
+        # Get VPC config
+        vpc_config = cluster.get("resourcesVpcConfig", {})
+        subnet_ids = sorted(vpc_config.get("subnetIds", []))
+        security_group_ids = sorted(vpc_config.get("securityGroupIds", []))
+        endpoint_public = vpc_config.get("endpointPublicAccess", True)
+        endpoint_private = vpc_config.get("endpointPrivateAccess", False)
+
+        # Get logging config
+        logging_config = cluster.get("logging", {}).get("clusterLogging", [])
+        enabled_log_types = []
+        for log_group in logging_config:
+            if log_group.get("enabled"):
+                enabled_log_types.extend(log_group.get("types", []))
+        enabled_log_types = sorted(enabled_log_types)
+
+        # Get encryption config
+        encryption_config = cluster.get("encryptionConfig", [])
+        encryption_enabled = len(encryption_config) > 0
+
+        # Get node groups count (detect manually added node groups)
+        nodegroup_count = 0
+        try:
+            ng_resp = eks.list_nodegroups(clusterName=cluster_name)
+            nodegroup_count = len(ng_resp.get("nodegroups", []))
+        except Exception:
+            pass
+
+        # Get addons (detect manually installed addons)
+        addon_names = []
+        try:
+            addon_resp = eks.list_addons(clusterName=cluster_name)
+            addon_names = sorted(addon_resp.get("addons", []))
+        except Exception:
+            pass
+
         attributes = {
             "name": cluster.get("name"),
             "version": cluster.get("version"),
             "status": cluster.get("status"),
             "platform_version": cluster.get("platformVersion"),
             "role_arn": cluster.get("roleArn"),
+            "subnet_ids": subnet_ids,
+            "security_group_ids": security_group_ids,
+            "endpoint_public_access": endpoint_public,
+            "endpoint_private_access": endpoint_private,
+            "enabled_log_types": enabled_log_types,
+            "encryption_enabled": encryption_enabled,
+            "nodegroup_count": nodegroup_count,
+            "addon_names": addon_names,
         }
 
         return ResourceMetadata(
@@ -884,10 +985,26 @@ class AWSProvider:
         tags_response = sns.list_tags_for_resource(ResourceArn=topic_arn)
         tags = self._aws_tags_to_dict(tags_response.get("Tags", []))
 
+        # Get subscription count (detect manually added subscriptions)
+        subscription_count = 0
+        try:
+            sub_resp = sns.list_subscriptions_by_topic(TopicArn=topic_arn)
+            subscription_count = len(sub_resp.get("Subscriptions", []))
+        except Exception:
+            pass
+
+        # Get delivery policy and access policy
+        has_access_policy = bool(attrs.get("Policy", ""))
+        delivery_policy = attrs.get("DeliveryPolicy", "")
+
         attributes = {
             "display_name": attrs.get("DisplayName", ""),
             "kms_master_key_id": attrs.get("KmsMasterKeyId", ""),
             "fifo_topic": attrs.get("FifoTopic", "false") == "true",
+            "content_based_deduplication": attrs.get("ContentBasedDeduplication", "false") == "true",
+            "subscription_count": subscription_count,
+            "has_access_policy": has_access_policy,
+            "has_delivery_policy": bool(delivery_policy),
         }
 
         return ResourceMetadata(
